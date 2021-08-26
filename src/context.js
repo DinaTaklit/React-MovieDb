9b66f625f2df83f832895c3bd0195dc0407c2a25
DinaTaklit <ez_taklit@esi.dz>
2021-08-26 22:45:12 +0100

    style: context: add childern to App provider

--- src/context.js
+++ src/context.js
@@ -1,13 +1,27 @@
-import React, {useContext} from 'react'
-
+import React, {useContext, useState, useEffect} from 'react'
+import {fetchMovies} from '../src/services'
 
 const AppContext = React.createContext()
 
 export const AppProvider = ({children}) => {
+
+    // Global state for now playing movies
+    const [nowPlayingMovies, setNowPlayingMovies] = useState([])
+
+
+    useEffect(() => {
+        const fetchAPI = async() =>{
+            setNowPlayingMovies(await fetchMovies())
+        }
+        fetchAPI()
+    }, [])
+    
+
     return (
         <AppContext.Provider value={{
-
+            nowPlayingMovies
         }}>
+            {children}
         </AppContext.Provider>
     )
 }
