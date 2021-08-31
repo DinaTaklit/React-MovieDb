f76e4ddcfef38007299d2c66ade1bfc524cc2f7c
DinaTaklit <ez_taklit@esi.dz>
2021-08-31 14:17:59 +0100

    feat:Compnents: MovieCarousel: implement MovieCarousel Compoenet

---
+++ src/components/MovieCarousel/MovieCarousel.js
@@ -0,0 +1,32 @@
+import React,{useReducer} from 'react'
+import { useGlobalContext } from '../../context';
+import './MovieCarousel.scss'
+import reducer from './reducer.js'
+import Slide from './Slide'
+
+function MovieCrousel({offset}) {
+    // Get global context 
+    const {nowPlayingMovies} = useGlobalContext()
+
+    // Reducer inital state for the carousel
+    const initialState = {
+      slideIndex: 0,
+      slideLength: nowPlayingMovies.length,
+    };
+
+    const [state, dispatch] = useReducer(reducer, initialState)
+    
+    return (
+      <div className="slides">
+        <button onClick={() => dispatch({ type: "PREV" })}>‹</button>    
+          {[...nowPlayingMovies, ...nowPlayingMovies, ...nowPlayingMovies].map((movie, i) => {
+            let offset = nowPlayingMovies.length + (state.slideIndex - i);
+            return <Slide slide={movie} offset={offset} key={i} />;
+          })}
+        <button onClick={() => dispatch({ type: "NEXT" })}>›</button>
+      </div>
+    )
+    
+}
+
+export default MovieCrousel
