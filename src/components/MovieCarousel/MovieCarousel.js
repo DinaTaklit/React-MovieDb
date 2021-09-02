cd97b4d8c208fa3869c820c9c82ac2bb72f146b6
DinaTaklit <ez_taklit@esi.dz>
2021-09-02 09:19:55 +0100

    feat: MovieCarousel: implement the component

--- src/components/MovieCarousel/MovieCarousel.js
+++ src/components/MovieCarousel/MovieCarousel.js
@@ -1,33 +1,78 @@
-import React,{useReducer} from 'react'
-import { useGlobalContext } from '../../context';
+import { Link } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import './MovieCarousel.scss'
-import reducer from './reducer.js'
-import Slide from './Slide'
 
-function MovieCrousel({offset}) {
-    // Get global context 
-    const {nowPlayingMovies} = useGlobalContext()
+function MovieCarousel({movies}) {
 
-    // Reducer inital state for the carousel
-    const initialState = {
-      slideIndex: 0,
-      slideLength: nowPlayingMovies.length,
+    const movieList = movies.map((movie) => {
+        const {id, title, poster, rating} = movie;
+        return (
+          <div key={id} className="movie-wrapper">
+            <div className="card">
+              <Link to={`/movie/${id}`}>
+                <img className="img-fluid" src={poster} alt={title}></img>
+              </Link>
+            </div>
+            <div className="mt-2">
+              <p style={{ fontWeight: "bolder" }}>{title}</p>
+              <p>Rated: {rating}</p>
+              <ReactStars
+                count={rating}
+                size={20}
+                color1={"#f4c10f"}
+              ></ReactStars>
+            </div>
+          </div>
+        );
+    });
+
+    const responsive = {
+      superLargeDesktop: {
+        // the naming can be any, depends on you.
+        breakpoint: { max: 4000, min: 3000 },
+        items: 5
+      },
+      desktop: {
+        breakpoint: { max: 3000, min: 1024 },
+        items: 4
+      },
+      tablet: {
+        breakpoint: { max: 1024, min: 768 },
+        items: 2
+      },
+      smallTablet: {
+        breakpoint: { max: 768, min: 464 },
+        items: 2
+      },
+      Mobile: {
+        breakpoint: { max: 464, min: 0 },
+        items: 1
+      }
     };
 
-    // Use use reducer to dispatch events and take actions
-    const [state, dispatch] = useReducer(reducer, initialState)
-    
     return (
-      <div className="slides py-4">
-        <button onClick={() => dispatch({ type: "PREV" })}>‹</button>    
-          {[...nowPlayingMovies, ...nowPlayingMovies, ...nowPlayingMovies].map((movie, i) => {
-            let offset = nowPlayingMovies.length + (state.slideIndex - i);
-            return <Slide slide={movie} offset={offset} key={i} />;
-          })}
-        <button onClick={() => dispatch({ type: "NEXT" })}>›</button>
+      <div className="row mt-3">
+        <div className="col">
+        <Carousel 
+          responsive={responsive}
+          // draggable={true}
+          // infinite={true}
+          // autoPlay={true}
+          // autoPlaySpeed={1000}
+          // keyBoardControl={true}
+          // customTransition="all .5"
+          // transitionDuration={500}
+          // containerClass="carousel-container"
+          // removeArrowOnDeviceType={["tablet", "mobile"]}
+          // itemClass="carousel-item-padding-40-px"
+        >
+          {movieList}
+        </Carousel>
+        </div>
       </div>
     )
-    
 }
 
-export default MovieCrousel
+export default MovieCarousel
