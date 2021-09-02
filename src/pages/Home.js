74c54ba13a3a2ed462bbb1d64379ab422b89de9f
DinaTaklit <ez_taklit@esi.dz>
2021-09-02 09:17:47 +0100

    feat: Home: add global context: movies by genre

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
+import { useGlobalContext } from '../context'
+import MovieCarouselHeader from '../components/MovieCarouselHeader/MovieCarouselHeader'
+import GenreList from '../components/GenreList/GenreList'
+import MovieCarousel from '../components/MovieCarousel/MovieCarousel'
+
 function Home() {
+
+    // Get global context 
+    const{loading, moviesByGenre}= useGlobalContext()
+        
+    if(loading) {
+        return (
+            <div className="container">
+                Loading...
+            </div>
+        )
+    }
     return (
-        <div>
-            Home
+        <div className="container">
+            <MovieCarouselHeader/>
+            <GenreList/> 
+            <MovieCarousel movies={moviesByGenre}/>
         </div>
     )
 }
