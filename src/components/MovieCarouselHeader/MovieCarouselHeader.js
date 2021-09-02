4ea40c76b0ce402554c728f10f125236cb1bb78f
DinaTaklit <ez_taklit@esi.dz>
2021-09-02 09:18:41 +0100

    style: change the name of MovieCarousel to MovieCarouselHeader since it is used on the header

---
+++ src/components/MovieCarouselHeader/MovieCarouselHeader.js
@@ -0,0 +1,33 @@
+import React,{useReducer} from 'react'
+import { useGlobalContext } from '../../context';
+import './MovieCarouselHeader.scss'
+import reducer from './reducer.js'
+import Slide from './Slide'
+
+function MovieCrousel({offset}) {
+    // Get global context 
+    const {nowPlayingMovies} = useGlobalContext()
+
+    // Reducer inital state for the carousel
+    const initialState = {
+      slideIndex: 0,
+      slideLength: nowPlayingMovies.length,
+    };
+
+    // Use use reducer to dispatch events and take actions
+    const [state, dispatch] = useReducer(reducer, initialState)
+    
+    return (
+      <div className="slides py-4">
+        <button onClick={() => dispatch({ type: "PREV" })}>‹</button>    
+          {[...nowPlayingMovies, ...nowPlayingMovies, ...nowPlayingMovies].map((movie, i) => {
+            let offset = nowPlayingMovies.length + (state.slideIndex - i);
+            return <Slide slide={movie} offset={offset} key={i} />;
+          })}
+        <button onClick={() => dispatch({ type: "NEXT" })}>›</button>
+      </div>
+    )
+    
+}
+
+export default MovieCrousel
---
+++ src/components/MovieCarouselHeader/Slide.js
@@ -0,0 +1,80 @@
+import React from 'react'
+
+function Slide({ slide, offset }) {
+    const active = offset === 0 ? true : null;
+    const ref = useTilt(active);
+
+    function useTilt(active) {
+        const ref = React.useRef(null);
+      
+        React.useEffect(() => {
+          if (!ref.current || !active) {
+            return;
+          }
+      
+          const state = {
+            rect: undefined,
+            mouseX: undefined,
+            mouseY: undefined
+          };
+      
+          let el = ref.current;
+      
+          const handleMouseMove = (e) => {
+            if (!el) {
+              return;
+            }
+            if (!state.rect) {
+              state.rect = el.getBoundingClientRect();
+            }
+            state.mouseX = e.clientX;
+            state.mouseY = e.clientY;
+            const px = (state.mouseX - state.rect.left) / state.rect.width;
+            const py = (state.mouseY - state.rect.top) / state.rect.height;
+      
+            el.style.setProperty("--px", px);
+            el.style.setProperty("--py", py);
+          };
+      
+          el.addEventListener("mousemove", handleMouseMove);
+      
+          return () => {
+            el.removeEventListener("mousemove", handleMouseMove);
+          };
+        }, [active]);
+      
+        return ref;
+    }
+  
+    return (
+      <div
+        ref={ref}
+        className="slide"
+        data-active={active}
+        style={{
+          "--offset": offset,
+          "--dir": offset === 0 ? 0 : offset > 0 ? 1 : -1
+        }}
+      >
+        <div
+          className="slideBackground"
+          style={{
+            backgroundImage: `url('${slide.backPoster}')`
+          }}
+        />
+        <div
+          className="slideContent"
+          style={{
+            backgroundImage: `url('${slide.backPoster}')`,
+            objectFit: 'cover'
+          }}
+        >
+          <div className="slideContentInner">
+            <h2 className="slideTitle">{slide.title}</h2>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Slide
