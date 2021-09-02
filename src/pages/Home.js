6e1c52c2b244205cd0bd0759cf0be2445ac1c6d1
DinaTaklit <ez_taklit@esi.dz>
2021-09-02 14:01:19 +0100

    feat: Home: add the trending movies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,11 @@ import { useGlobalContext } from '../context'
 import MovieCarouselHeader from '../components/MovieCarouselHeader/MovieCarouselHeader'
 import GenreList from '../components/GenreList/GenreList'
 import MovieCarousel from '../components/MovieCarousel/MovieCarousel'
-
+import PersonsCarousel from '../components/PersonsCarousel/PersonsCarousel'
 function Home() {
 
     // Get global context 
-    const{loading, moviesByGenre}= useGlobalContext()
+    const{loading, moviesByGenre, trendingPersons}= useGlobalContext()
         
     if(loading) {
         return (
@@ -20,6 +20,7 @@ function Home() {
             <MovieCarouselHeader/>
             <GenreList/> 
             <MovieCarousel movies={moviesByGenre}/>
+            <PersonsCarousel persons={trendingPersons}/>
         </div>
     )
 }
