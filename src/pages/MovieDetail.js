d7846cfda662f00dedfa9d4aef3bda784c7c5cce
DinaTaklit <ez_taklit@esi.dz>
2021-11-06 16:30:43 +0100

    feat: Movie Detai: add similar movies

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -4,11 +4,13 @@ import MovieDetailHeader from '../components/MovieDetail/MovieDetailHeader'
 import GenreList from '../components/GenreList/GenreList'
 import MovieDetailInfo from '../components/MovieDetail/MovieDetailInfo'
 import PersonsCarousel from '../components/PersonsCarousel/PersonsCarousel'
+import MovieCarousel from '../components/MovieCarousel/MovieCarousel'
 
 import {
     fetchMovieDetail,
     fetchMovieVideo,
     fetchMovieCasts,
+    fetchSimilarMovies,
 } from '../services'
 
 function MovieDetail({match}) {
@@ -28,12 +30,16 @@ function MovieDetail({match}) {
     // Movie casts state
     const [movieCasts, setMovieCasts] = useState([]);
 
+    // Similar movies state
+    const [similarMovies, setSimilarMovies] = useState([]);
+
     useEffect(() => {
         setLoading(true)
         const fetchAPI = async() =>{
             setMovieDetail(await fetchMovieDetail(movieId))
             setPosterVideo( await fetchMovieVideo(movieId))
             setMovieCasts(await fetchMovieCasts(movieId))
+            setSimilarMovies(await fetchSimilarMovies(movieId))
         }
         fetchAPI()
         setLoading(false)
@@ -63,6 +69,13 @@ function MovieDetail({match}) {
                 <p className="title">CASTS</p>
                 <PersonsCarousel persons={movieCasts}/>
             </div>
+
+            {/* Similar movies */}
+            <div className="mt-3">
+                <p className="title">SIMILAR MOVIES</p>
+                <MovieCarousel movies={similarMovies}/>
+            </div>
+
         </main>
     )
 }
