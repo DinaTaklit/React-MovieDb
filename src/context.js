5bc68da25b0bf549e2250f7426053829a3f8bfde
DinaTaklit <ez_taklit@esi.dz>
2021-11-06 06:23:30 +0100

    feat:Context: remove home state from context

--- src/context.js
+++ src/context.js
@@ -1,15 +1,5 @@
-import React, {useContext, useState, useEffect} from 'react'
-import {
-    fetchMovies,
-    fetchGenres,
-    fetchMoviesByGenre,
-    fetchTrendingPersons,
-    fetchTopRatedMovies,
-    fetchMovieDetail,
-    fetchMovieVideos,
-    fetchMovieCasts,
-    fetchSimilarMovies,
-} from '../src/services'
+import React, {useContext, useState} from 'react'
+
 
 const AppContext = React.createContext()
 
@@ -17,16 +7,6 @@ export const AppProvider = ({children}) => {
 
     // Loading
     const [loading, setLoading] = useState(true)
-    // Global state for now playing movies
-    const [nowPlayingMovies, setNowPlayingMovies] = useState([])
-    // Global state for genres
-    const [genres, setGenres] = useState([]);
-    // Global state for movies by genre
-    const [moviesByGenre, setMoviesByGenre] = useState([]);
-    // Global state for trending persons
-    const [trendingPersons, setTrendingPersons] = useState([]);
-    // Global state for top rated movies
-    const [topRatedMovies, setTopRatedMovies] = useState([]);
     // Global state for movie detail
     const [movieDetail, setMovieDetail] = useState({});
     // Global state for movie videos
@@ -36,36 +16,23 @@ export const AppProvider = ({children}) => {
     // Global state for similar movies
     const [similarMovies, setSimilarMovies] = useState([]);
 
-    useEffect(() => {
-        setLoading(true)
-        const fetchAPI = async() =>{
-            setNowPlayingMovies(await fetchMovies())
-            setGenres(await fetchGenres())
-            setMoviesByGenre(await fetchMoviesByGenre())
-            setTrendingPersons(await fetchTrendingPersons())
-            setTopRatedMovies(await fetchTopRatedMovies())
-            setMovieDetail(await fetchMovieDetail())
-            setMovieVideos(await fetchMovieVideos())
-            setMovieCasts(await fetchMovieCasts())
-            setSimilarMovies(await fetchSimilarMovies())
-            setLoading(false)
-        }
-        fetchAPI()
-    }, [])
-    
+    // Global state to get params (id) from url for single movie
+    const [movieId, setMovieId] = useState(null);
+
 
     return (
         <AppContext.Provider value={{
             loading,
-            nowPlayingMovies,
-            genres,
-            moviesByGenre,
-            trendingPersons,
-            topRatedMovies,
+            setLoading,
             movieDetail,
             movieVideos,
             movieCasts,
             similarMovies,
+            setMovieId,
+            setMovieDetail,
+            setMovieVideos,
+            setMovieCasts,
+            setSimilarMovies,
         }}>
             {children}
         </AppContext.Provider>
