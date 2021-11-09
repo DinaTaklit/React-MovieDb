a6126b9b03b0016c9471672e17c1a6506f052593
DinaTaklit <ez_taklit@esi.dz>
2021-11-09 21:43:35 +0100

    feat:Loading:layout

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,15 @@
+import './Loading.scss'
+function Loading() {
+    return (
+        <div className="lading-container">
+            <div class="loading">
+                <div class="ring"></div>
+                <div class="ring"></div>
+                <div class="ring"></div>
+                <p>Loading...</p>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
